38703d5f8c04d2caf7156869348a99dcee3b9585
MuratAcar00 <muratacar1152@gmail.com>
2025-08-09 10:54:51 +0300

    Analysis of Categorical Variables 2

--- 8)advancedFunctionalEda/advancedFunctionalEda.py
+++ 8)advancedFunctionalEda/advancedFunctionalEda.py
@@ -114,3 +114,35 @@ cat_summary(df, "sex")
 
 for col in cat_cols:
     cat_summary(df, col)
+
+#Aynı fonksiyonun sütun grafikli hali
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                       "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("************************")
+
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show(block=True)
+
+cat_summary(df, "sex",plot=True)
+
+for col in cat_cols:
+    if df[col].dtypes=="bool":
+        print("Type is bool")
+    else:
+        cat_summary(df, col,plot=True)
+
+df["adult_male"].astype(int)
+
+
+cat_summary(df, "sex",plot=True)
+
+for col in cat_cols:
+    if df[col].dtypes=="bool":
+        df[col]=df[col].astype(int)
+        cat_summary(df, col, plot=True)
+    else:
+        cat_summary(df, col,plot=True)
+
+
